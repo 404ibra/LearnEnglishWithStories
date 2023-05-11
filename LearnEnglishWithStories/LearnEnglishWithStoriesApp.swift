6cdcd67b250139cc2151449b34756a141ea8e193
404ibra <ibra.aktass@gmail.com>
2023-05-11 16:33:49 +0300

    Initial Commit

---
+++ LearnEnglishWithStories/LearnEnglishWithStoriesApp.swift
@@ -0,0 +1,20 @@
+//
+//  LearnEnglishWithStoriesApp.swift
+//  LearnEnglishWithStories
+//
+//  Created by İbrahim Aktaş on 11.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct LearnEnglishWithStoriesApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}

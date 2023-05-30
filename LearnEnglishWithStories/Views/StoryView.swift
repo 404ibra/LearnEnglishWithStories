904d59d040bcb96aa07f8ac5040cec8e957de4da
404ibra <ibra.aktass@gmail.com>
2023-05-30 13:36:15 +0300

    sentence seperated by words

--- LearnEnglishWithStories/Views/StoryView.swift
+++ LearnEnglishWithStories/Views/StoryView.swift
@@ -2,30 +2,33 @@
 //  StoryView.swift
 //  LearnEnglishWithStories
 //
-//  Created by İbrahim Aktaş on 27.05.2023.
+//  Created by İbrahim Aktaş on 30.05.2023.
 //
 
 import SwiftUI
 
 struct StoryView: View {
     let sentence = "One nice morning the eggs hatched and out popped six chirply duckling."
-    let maxCharacterCountPerLine = 40 // Her satırda gösterilecek maksimum harf sayısı
+    let maxCharactersPerLine = 35
     
     var body: some View {
-        let characters = Array(sentence)
-        var lines: [[Character]] = []
-        var currentLine: [Character] = []
-        var currentCharacterCount = 0
+        let words = sentence.components(separatedBy: " ")
         
-        for character in characters {
-            if currentCharacterCount + 1 > maxCharacterCountPerLine {
-                lines.append(currentLine)
-                currentLine = []
-                currentCharacterCount = 0
-            }
+        var lines: [[String]] = []
+        var currentLine: [String] = []
+        var lineLength = 0
+        
+        for word in words {
+            let wordLength = word.count
             
-            currentLine.append(character)
-            currentCharacterCount += 1
+            if lineLength + wordLength > maxCharactersPerLine {
+                lines.append(currentLine)
+                currentLine = [word]
+                lineLength = wordLength + 1 // +1 for space
+            } else {
+                currentLine.append(word)
+                lineLength += wordLength + 1 // +1 for space
+            }
         }
         
         if !currentLine.isEmpty {
@@ -33,20 +36,27 @@ struct StoryView: View {
         }
         
         return VStack(alignment: .leading, spacing: 10) {
-            ForEach(lines, id: \.self) { line in
-                Text(String(line))
-                    .font(.system(size: 19, weight: .medium, design: .rounded))
-                    .onTapGesture {
-                        print("Tıklanan satır: \(String(line))")
+            ForEach(lines.indices, id: \.self) { lineIndex in
+                HStack(spacing: 5) {
+                    ForEach(lines[lineIndex], id: \.self) { word in
+                        Text(word)
+                            .font(.system(size: 19, weight: .medium, design: .rounded))
+                            .onTapGesture {
+                                print("Tıklanan kelime: \(word)")
+                            }
                     }
+                }
             }
         }
         .padding()
     }
 }
 
+
 struct StoryView_Previews: PreviewProvider {
     static var previews: some View {
         StoryView()
     }
 }
+
+

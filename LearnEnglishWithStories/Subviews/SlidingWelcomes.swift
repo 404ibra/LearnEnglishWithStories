b59f1caf3fdb2a2f10add74a472114f683e909eb
404ibra <ibra.aktass@gmail.com>
2023-06-23 23:38:23 +0300

    some edits code

---
+++ LearnEnglishWithStories/Subviews/SlidingWelcomes.swift
@@ -0,0 +1,20 @@
+//
+//  SlidingWelcomes.swift
+//  LearnEnglishWithStories
+//
+//  Created by İbrahim Aktaş on 23.06.2023.
+//
+
+import SwiftUI
+
+struct SlidingWelcomes: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SlidingWelcomes_Previews: PreviewProvider {
+    static var previews: some View {
+        SlidingWelcomes()
+    }
+}
--- LearnEnglishWithStories/ViewModes/AuthViewModel.swift
+++ LearnEnglishWithStories/ViewModes/AuthViewModel.swift
@@ -9,47 +9,60 @@ import Foundation
 import Firebase
 import FirebaseFirestoreSwift
 
-@MainActor
-
 
 protocol AuthenticationProtocol {
     func signIn(withEmail email: String, password: String) async throws
     func signUp(email: String, password: String, fullname: String) async throws
+    func signOut() async throws
 }
 
 protocol UserFetchProtocol {
     func fetchUser() async throws
 }
 
-
-
-
-class AuthViewModel: ObservableObject, AuthenticationProtocol, UserFetchProtocol {
+@MainActor
+class AuthViewModel: ObservableObject, AuthenticationProtocol {
+ 
     @Published var userSession: FirebaseAuth.User?
     @Published var currentUser: UserModel?
     
+   /* init(userSession: FirebaseAuth.User? ) {
+        self.userSession = Auth.auth().currentUser
+    }*/
 
     func signIn(withEmail email: String, password: String) async throws {
-        print("sign in")
+        do{
+            let result = try await Auth.auth().signIn(withEmail: email, password: password)
+            self.userSession = result.user
+            print("login yapıldı \(result.user.uid)")
+        } catch {
+            print("Debug : While login \(error.localizedDescription)")
+        }
     }
     
     func signUp(email: String, password: String, fullname: String) async throws {
-        let result = try await Auth.auth().createUser(withEmail: email, password: password)
-        self.userSession = result.user
-        
-        let user = UserModel(id: result.user.uid, fullName: fullname, isEmail: email, isPremium: false, lastStories: nil, favWords: nil, favStories: nil, medals: nil, level: nil, history: nil)
-        
-        let encodedUser = try Firestore.Encoder().encode(user)
-        try await Firestore.firestore().collection("Users").document(user.id).setData(encodedUser)
+        do {
+            let result = try await Auth.auth().createUser(withEmail: email, password: password)
+            self.userSession = result.user
+            let user = UserModel(id: result.user.uid, fullName: fullname, isEmail: email, isPremium: false, lastStories: nil, favWords: nil, favStories: nil, medals: nil, level: nil, history: nil)
+            let encodedUser = try Firestore.Encoder().encode(user)
+            try await Firestore.firestore().collection("Users").document(user.id).setData(encodedUser)
+        } catch {
+            
+        }
     }
     
-    func signAuth() async throws {
-        // Burada kullanıcı kimlik doğrulaması yapılabilir.
+    func signOut() async throws {
+        do {
+            try Auth.auth().signOut()
+            self.userSession = nil
+            self.currentUser = nil
+        } catch {
+            print("Debug: Failed to sign out with error : \(error.localizedDescription)")
+        }
     }
     
-    func fetchUser() async throws {
-        
-    }
+    
 }
 
 
